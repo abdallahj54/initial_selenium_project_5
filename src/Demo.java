f706be87e8f4844323e1a1d6e682b554cbd7f24f
Abdallah Jaber <abdallahj708@gmail.com>
2023-01-07 12:05:52 -0600

    updated code

--- src/Demo.java
+++ src/Demo.java
@@ -7,9 +7,14 @@ public class Demo {
         System.setProperty("webdriver.chrome.driver","/Users/abdallahjaber/IdeaProjects/initial_selenium_project_5/chromedriver");
 
         WebDriver driver = new ChromeDriver(); // Instantiating ChromeDriver
-        driver.get("https://www.google.com/"); // Navigate to our URL
 
         driver.manage().window().maximize(); // Opens our URL in FullScreen
+
+        driver.get("https://www.google.com/"); // Navigate to our URL
+
+        String title = driver.getTitle();
+
+        System.out.println(title);
 
         driver.quit(); // Quit the driver instance
     }
